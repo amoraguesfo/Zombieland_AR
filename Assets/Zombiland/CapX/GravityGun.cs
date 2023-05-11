709baef76931eb3cf10f0fcc709881f12d99dd60
toni <easyrobotics2@gmail.com>
2023-05-11 14:09:15 +0200

    Test gravityGun

--- Assets/Zombiland/CapX/GravityGun.cs
+++ Assets/Zombiland/CapX/GravityGun.cs
@@ -1,15 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
-public class GravityDown : MonoBehaviour
+public class GravityGun: MonoBehaviour
 {
     [SerializeField] Camera cam;
     [SerializeField] float maxGrabDistance = 10f, throwForce = 20f, lerpSpeed = 10f;
     [SerializeField] Transform objectHolder;
-
     Rigidbody grabbeddRB;
+    [SerializeField]
+    [Tooltip("Instanciates this prefab on a plane at the touch location")]
+    GameObject m_scene;
+       /// <summary>
+    /// Invoked whenever an object is placed in on a plane.
+    /// </summary>
+    public static event Action onPlacedObject;
+    public GameObject spawnedHoop { get; private set; }
+    private bool isPlaced = false;
 
+    ARRaycastManager m_RaycastManager;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
     void Start()
     {
         
@@ -17,31 +36,33 @@ public class GravityDown : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (grabbeddRB)
         {
-            if (grabbeddRB)
+            grabbeddRB.MovePosition(objectHolder.transform.position);
+
+        }
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
             {
-                grabbeddRB.MovePosition(objectHolder.transform.position);
-            }
-            if (grabbeddRB)
-            {
-                grabbeddRB.isKinematic = false;
-                grabbeddRB = null;
-            }
-            else
-            {
-                RaycastHit hit;
-                Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
-                if(Physics.Raycast(ray, out hit, maxGrabDistance))
-                {
-                    grabbeddRB = hit.collider.gameObject.GetComponent<Rigidbody>();
-                    if (grabbeddRB)
+
+                    if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
                     {
-                        grabbeddRB.isKinematic = true;
+                        Pose hitPose = s_Hits[0].pose;
+
+                        spawnedHoop = Instantiate(m_scene, hitPose.position, Quaternion.AngleAxis(180, Vector3.up));
+                        spawnedHoop.transform.parent = transform.parent;
+
+                        isPlaced = true;
+                        if (onPlacedObject != null)
+                        {
+                            onPlacedObject();
+                        }
                     }
                 }
             }
-
         }
-    }
+    
 }
